82540924259de7b4dc82a9512ac7a37538f3a40e
Akynyaz64 <akynyaz64@yandex.ru>
2021-07-10 14:16:30 +0300

    Static files are added

--- Handmade/home/views.py
+++ Handmade/home/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Index")
+    context = {}
+    return render(request, 'home/index.html', context)
